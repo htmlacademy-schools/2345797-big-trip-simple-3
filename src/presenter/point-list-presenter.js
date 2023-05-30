3a18363b856164a7dd6bd436d0dcb9a2ce20813d
Dafter <DafterPlay@mail.ru>
2023-05-30 14:10:18 +0300

    refactor

--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -5,18 +5,21 @@ import TripPointEditorView from '../view/trip-point-editor-view';
 import { render } from '../render';
 
 export default class PointListPresenter {
-  pointList = new TripListView();
+  #pointList = new TripListView();
+  #pointListContainer = null;
+  #pointsModel = null;
+  #pointListPoints = [];
 
   init(pointListContainer, pointsModel) {
-    this.pointListContainer = pointListContainer;
-    this.pointsModel = pointsModel;
-    this.pointListPoints = [...this.pointsModel.getTasks()];
+    this.#pointListContainer = pointListContainer;
+    this.#pointsModel = pointsModel;
+    this.#pointListPoints = [...this.#pointsModel.points];
 
-    render(this.pointList, this.pointListContainer);
-    render(new TripPointEditorView(this.pointListPoints[0]), this.pointList.getElement());
-    render(new TripPointCreatorView, this.pointList.getElement());
-    for (let i = 0; i < this.pointListPoints.length; i++) {
-      render(new TripPointView(this.pointListPoints[i]), this.pointList.getElement());
+    render(this.#pointList, this.#pointListContainer);
+    render(new TripPointEditorView(this.#pointListPoints[0]), this.#pointList.element);
+    render(new TripPointCreatorView, this.#pointList.element);
+    for (let i = 0; i < this.#pointListPoints.length; i++) {
+      render(new TripPointView(this.#pointListPoints[i]), this.#pointList.element);
     }
   }
 }
