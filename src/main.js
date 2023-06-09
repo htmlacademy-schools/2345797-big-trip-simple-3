1b526a4956172be1dc7c3ffdfe04bb2732eadee4
Dafter <DafterPlay@mail.ru>
2023-06-09 14:50:09 +0300

    7.8

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import FilterView from './view/filter-view.js';
 import PointListPresenter from './presenter/point-list-presenter.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
+import { FilterType } from './const.js';
 
 const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
@@ -11,6 +12,6 @@ const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
 const pointListPresenter = new PointListPresenter(tripEventsElement, pointsModel);
 
-render(new FilterView(), tripControlsFiltersElement);
+render(new FilterView(FilterType.EVERYTHING), tripControlsFiltersElement);
 
 pointListPresenter.init();
